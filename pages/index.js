ecd06aed1639d1328ab1bab6615ffd80255ace88
Stefan Redfield <redfield.stefan@gmail.com>
2017-03-21 19:41:43 -0700

    add more slides

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,15 @@ import { prefixLink } from 'gatsby-helpers';
 import Helmet from 'react-helmet';
 import { config } from 'config';
 
-import capstanBacksplash from "../assets/images/Capstan-8x8-backsplash.jpg";
-import percevalFloor from "../assets/images/Perceval-8x8-floor.jpg";
 import seresGrand from "../assets/images/Seres-2-Grand.jpg";
 import interlacedLeaves from "../assets/images/Interlaced-Leaves1.jpg";
 import camelot from "../assets/images/Camelot2.jpg";
+import greenBlossomShower from "../assets/images/green-blossom-shower.jpg";
+import greenBlossomBathroom from "../assets/images/green-blossom-bathroom.jpg";
+import whiteBlossomShower from "../assets/images/white-blossom-shower.jpg";
+import kitchenSink from "../assets/images/kitchen-sink.jpg";
+import blackStoveTop from "../assets/images/black-stove-top.jpg";
+import dragonFlySink from "../assets/images/dragon-fly-sink.jpg";
 import homerCafe from "../assets/images/homer-cafe.jpg";
 import beachStyle from "../assets/images/beach-style-staircase.jpg";
 import prado from "../assets/images/prado.jpg";
@@ -41,13 +45,6 @@ export default class Index extends React.Component {
             Joining forces with Fireclay Tile in 2014, the infusion of the <span className="kibak-tile">Kibak</span> point of view and designs brought a fresh perspective to Fireclay Tile and was an instrumental force in their new look and brand aesthetic.
           </p>
 
-          <ul className="core-list strong">
-            <li>We are passionate about listening</li>
-            <li>We bring over forty years of knowledge and competence to each project</li>
-            <li>Collaboration and teamwork drive our creative innovation</li>
-            <li>Obsessively chase the perfect color palette</li>
-          </ul>
-
           <p>
             2017 has brought new changes at <span className="kibak-tile">Kibak Tile</span>.  Susanne’s daughter has joined the team. With a love of tile deeply imprinted since childhood, Carli’s command and skill level in both product development and outreach injects a fresh and welcome viewpoint and enthusiasm.
           </p>
@@ -55,16 +52,28 @@ export default class Index extends React.Component {
           <p>
             Also joining team <span className="kibak-tile">Kibak</span> this year is talented young designer, Mari Burck.  Mari brings a modern, Nordic sensibility to her original tile designs. Mari’s visionary scope and flawless creative instincts  make her a natural fit at <span className="kibak-tile">Kibak Tile</span>.
           </p>
+
+          <ul className="core-list strong">
+            <li>We are passionate about listening</li>
+            <li>We bring over forty years of knowledge and competence to each project</li>
+            <li>Collaboration and teamwork drive our creative innovation</li>
+            <li>Obsessively chase the perfect color palette</li>
+          </ul>
+
+          <a className="btn" href="mailto:sales@kibaktile.com">Contact Us</a>
+
         </section>
 
         <section className="tiles-main">
           <Carousel className="carousel" slidesToShow={3} slidesToScroll={1} wrapAround={true} cellSpacing={20}>
-            <img className="tiles-main-img" src={beachStyle} alt="Perceval 8 by 8 Floor Pattern" />
-            <img className="tiles-main-img" src={prado} alt="Perceval 8 by 8 Floor Pattern" />
-            <img className="tiles-main-img" src={homerCafe} alt="Perceval 8 by 8 Floor Pattern" />
-            <img className="tiles-main-img" src={beachStyle} alt="Perceval 8 by 8 Floor Pattern" />
-            <img className="tiles-main-img" src={prado} alt="Perceval 8 by 8 Floor Pattern" />
-            <img className="tiles-main-img" src={homerCafe} alt="Perceval 8 by 8 Floor Pattern" />
+            <img className="tiles-main-img" src={beachStyle} alt="Beach Style" />
+            <img className="tiles-main-img" src={prado} alt="Prado" />
+            <img className="tiles-main-img" src={homerCafe} alt="Homer Cafe" />
+            <img className="tiles-main-img" src={greenBlossomShower} alt="Green Blossom Shower" />
+            <img className="tiles-main-img" src={kitchenSink} alt="Kitchen Sink" />
+            <img className="tiles-main-img" src={whiteBlossomShower} alt="White Blossom Shower" />
+            <img className="tiles-main-img" src={dragonFlySink} alt="Dragon Fly Sink" />
+            <img className="tiles-main-img" src={blackStoveTop} alt="Stove Top" />
           </Carousel>
         </section>
 
