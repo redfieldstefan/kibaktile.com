47a4dacf99ede208710336dd9b2b8711e8d00e07
Stefan Redfield <redfield.stefan@gmail.com>
2017-09-18 21:35:35 -0700

    add secret cherry page

--- components/feature-page.jsx
+++ components/feature-page.jsx
@@ -4,15 +4,37 @@ import React, { Component } from "react";
 
 export default class FeaturePage extends Component {
   render() {
-    const { description, name, installs } = this.props.pattern;
+    const { background, description, features, installs, name, steps, url } = this.props.pattern;
 
     return (
       <BasePage className="FeaturePage">
-        <div className="feature-page-hero" style={{backgroundImage: `url(${installs[0]})`}}>
-          <h1>{name}</h1>
-          <p>{description}</p>
+        <div className="feature-page-hero" style={{backgroundImage: `url(${background})`}}>
+          <div className="pattern-breadcrumb">
+            <a href="/catalog/">Patterns</a>
+            <span className="divider">/</span>
+            <a href={url}>{name}</a>
+          </div>
+          <span className="vertical-center">
+            <div className="header-container">
+              <h1 className="">{name}</h1>
+              <div className="feature-page-links">
+                <a href="#installations">Installations</a>
+                <a href="#process">Process</a>
+                <a href="#contact">Contact</a>
+              </div>
+            </div>
+          </span>
         </div>
-        <div className="pattern-carousel">
+        <div className="feature-page-description">
+          <p className="feature-page-paragraph">{description}</p>
+          <ul>
+            {
+              features &&
+              features.map((step, i) => <li key={i}>{step}</li>)
+            }
+          </ul>
+        </div>
+        <div id="installations" className="feature-pattern-carousel">
           <Carousel emulateTouch showStatus={false} infiniteLoop useKeyboardArrows>
             { installs.map((image) => {
               return (
@@ -22,6 +44,30 @@ export default class FeaturePage extends Component {
               );
             })}
           </Carousel>
+        </div>
+        <div id="process" className="feature-page-process">
+          <h2>Process</h2>
+          <ul>
+            <li>
+              <a href="#contact">Contact Us</a>
+            </li>
+            {
+              steps &&
+              steps.map((step, i) => <li key={i}>{step}</li> )
+            }
+          </ul>
+        </div>
+        <div id="contact" className="feature-page-contact">
+          <form action="https://formspree.io/redfield.stefan@gmail.com" method="post" enctype="text/plain">
+            <span className="name">
+              <input type="text" placeholder="Name" name="name" />
+            </span>
+            <span className="email">
+              <input type="text" placeholder="Email" name="mail" />
+            </span>
+            <input className="body" type="text" name="comment" size="50" />
+            <button className="submit" type="submit" value="Send">Send</button>
+          </form>
         </div>
       </BasePage>
     );
--- data/catalog/cherry-blossom/index.js
+++ data/catalog/cherry-blossom/index.js
@@ -1,9 +1,11 @@
 import squirrelTail1 from "./nw-squirrel-tail-1.jpg";
 import squirrelTail2 from "./nw-squirrel-tail-2.jpg";
 import squirrelTail3 from "./nw-squirrel-tail-3.jpg";
+import cherryBackground from "./cherry-background.jpg";
 
 export default {
   featured: true,
+  background: cherryBackground,
   name: "Cherry Blossom",
   description: `Add subtle texture and understated cool to any
     kitchen or bath. Gammon plays the field when it comes to
@@ -12,9 +14,21 @@ export default {
     more dramatic statement depending on dry-line and color
     choice.`,
   installs: [
-    squirrelTail2,
     squirrelTail1,
+    squirrelTail2,
     squirrelTail3
+  ],
+  features: [
+    "Made in Sisters Oregon",
+    "Hand painted and unique for every intallation",
+    "Custom colors and arrangement per project"
+  ],
+  steps: [
+    "Tell us about your project",
+    "We work with you to configure a design around the features of the room",
+    "Pick your desired colors",
+    "We screen, paint, and fire the tile",
+    "Your mural is shipped to you with instructions for installation"
   ],
   url: "/catalog/cherry-blossom/"
 };
