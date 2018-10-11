42c9cf143a4311396280a2dc918931fe3e230f1a
redfieldstefan <redfield.stefan@gmail.com>
2018-10-11 08:52:00 -0700

    wip add footer

---
+++ components/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import catalog from "../data/catalog";
+
+const Footer = () => (
+  <div className="Footer">
+    <div className="footer-catalog">
+      <ul className="footer-catalog-links">
+        <li>Some Phone Number</li>
+        <li>info@kibaktile.com</li>
+        <li>Some Address</li>
+      </ul>
+    </div>
+  </div>
+);
+
+export default Footer;
