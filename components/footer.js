1fb3a35a5ccfe04e8351b4a0be1a63de579b4222
Stefan Redfield <redfield.stefan@gmail.com>
2018-10-15 20:06:28 -0700

    design footer

--- components/footer.js
+++ components/footer.js
@@ -2,15 +2,10 @@ import React from "react";
 import catalog from "../data/catalog";
 
 const Footer = () => (
-  <div className="Footer">
-    <div className="footer-catalog">
-      <ul className="footer-catalog-links">
-        <li>Some Phone Number</li>
-        <li>info@kibaktile.com</li>
-        <li>Some Address</li>
-      </ul>
-    </div>
-  </div>
+  <ul className="Footer">
+    <li><a href="mailto:info@kibaktile.com">info@kibaktile.com</a></li>
+    <li><a href="https://goo.gl/maps/BcbCzHMETLw" target="blank">183 E.Hood St Sisters, Oregon</a></li>
+  </ul>
 );
 
 export default Footer;
