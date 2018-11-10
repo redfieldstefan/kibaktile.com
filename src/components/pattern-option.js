ccee621e29c2601e1a1eda12b5c810deb9ba082c
Stefan Redfield <redfield.stefan@gmail.com>
2018-11-10 09:26:23 -0800

    remove price from catalog

--- src/components/pattern-option.js
+++ src/components/pattern-option.js
@@ -2,39 +2,15 @@ import React, { Component } from "react";
 import Price from "./price";
 import {calculateCost} from "../helpers/price-groups";
 
-class PatternOption extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.selectPattern = this.selectPattern.bind(this);
-  }
-
-  selectPattern() {
-    return this.props.onClick(this.props.pattern);
-  }
-
-  render() {
-    const { pattern } = this.props;
-
-    return (
-      <a href={pattern.url} className="PatternOption" onClick={this.selectPattern}>
-        <img className="pattern-option-line-drawing" src={pattern.painted || pattern.lineDrawing} />
-        <div className="pattern-option-details">
-          <span className="pattern-option-name">
-            {pattern.name}
-          </span>
-          <div className="pattern-price-container">
-            {
-              pattern.dimensions &&
-              pattern.priceGroup &&
-              pattern.dimensions.map((dimension) => <Price key={`${pattern.name}-${dimension}`} cost={calculateCost(pattern.priceGroup, dimension)} dimension={dimension} />)
-            }
-          </div>
-        </div>
-      </a>
-    );
-  }
-};
+const PatternOption = ({pattern}) => (
+  <a href={pattern.url} className="PatternOption">
+    <img className="pattern-option-line-drawing" src={pattern.painted || pattern.lineDrawing} />
+    <div className="pattern-option-details">
+      <span className="pattern-option-name">
+        {pattern.name}
+      </span>
+    </div>
+  </a>
+);
 
 export default PatternOption;
--- src/pages/catalog/index.js
+++ src/pages/catalog/index.js
@@ -6,47 +6,25 @@ import catalog from "../../data/catalog";
 
 const pdfUrl = "https://s3-us-west-2.amazonaws.com/studio-redfield/2018+Kibak+Tile+catalog.pdf";
 
-class Catalog extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      catalog,
-      selectedPattern: catalog[0]
-    };
-
-    this.selectPattern = this.selectPattern.bind(this);
-  }
-
-  selectPattern(pattern) {
-    this.setState({
-      selectedPattern: pattern
-    });
-  }
-
-  render() {
-    return (
-      <BasePage className="Catalog" title="Kibak Tile | Catalog">
-        <div className="download-catalog-bar">
-          <p>
-            Browse a selection of our patterns online or
-            <a className="download-link color-dark-mint" href={pdfUrl}>
-              Download Full Catalog PDF
-            </a>
-          </p>
-        </div>
-        <div className="catalog-options">
-          {
-            catalog.map((pattern, i) => (
-              <PatternOption pattern={pattern} key={i} onClick={this.selectPattern} />
-            ))
-          }
-        </div>
-      </BasePage>
-    );
-  }
-}
+const Catalog = () => (
+  <BasePage className="Catalog" title="Kibak Tile | Catalog">
+    <div className="download-catalog-bar">
+      <p>
+        Browse a selection of our patterns online or
+        <a className="download-link color-dark-mint" href={pdfUrl}>
+          Download Full Catalog PDF
+        </a>
+      </p>
+    </div>
+    <div className="catalog-options">
+      {
+        catalog.map((pattern, i) => (
+          <PatternOption pattern={pattern} key={i} />
+        ))
+      }
+    </div>
+  </BasePage>
+);
 
 
 export default Catalog;
