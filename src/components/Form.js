03876a29dea983c739ea16ce6d36e833192eead4
Stefan Redfield <redfield.stefan@gmail.com>
2018-11-21 08:49:55 -0800

    fix form email address

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,7 @@ import classnames from 'classnames';
 import Button from "./Button";
 
 const Form = ({onSubmit, children, className}) => (
-	<form className={classnames("Form", className)} action="https://formspree.io/redfield.stefan@gmail.com" method="POST">
+	<form className={classnames("Form", className)} action="https://formspree.io/info@kibaktile.com" method="POST">
 		{children}
 		<Button>Send</Button>
 	</form>
